2d4861cba4d5bd5fd74785896c0353d38978988f
Long Tran Quang <longtrq26@gmail.com>
2026-01-14 17:31:45 +0700

    feat(security): add rate limiting with throttler

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccessTokenGuard } from './common/guards/access-token.guard';
 import { RolesGuard } from './common/guards/roles.guard';
@@ -9,11 +10,11 @@ import { AuthModule } from './modules/auth/auth.module';
 import { BookingModule } from './modules/booking/booking.module';
 import { CourtModule } from './modules/court/court.module';
 import { NotificationModule } from './modules/notification/notification.module';
+import { PaymentModule } from './modules/payment/payment.module';
 import { UserModule } from './modules/user/user.module';
+import { VenueModule } from './modules/venue/venue.module';
 import { LoggerModule } from './providers/logger/logger.module';
 import { SmtpModule } from './providers/smtp/smtp.module';
-import { VenueModule } from './modules/venue/venue.module';
-import { PaymentModule } from './modules/payment/payment.module';
 
 @Module({
   imports: [
@@ -28,6 +29,22 @@ import { PaymentModule } from './modules/payment/payment.module';
       useFactory: (config: ConfigService) => config.getOrThrow('database'),
     }),
 
+    ThrottlerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          name: 'short',
+          ttl: config.getOrThrow('rateLimit.short.ttl'),
+          limit: config.getOrThrow('rateLimit.short.limit'),
+        },
+        {
+          name: 'long',
+          ttl: config.getOrThrow('rateLimit.long.ttl'),
+          limit: config.getOrThrow('rateLimit.long.limit'),
+        },
+      ],
+    }),
+
     LoggerModule,
     SmtpModule,
     NotificationModule,
@@ -39,6 +56,10 @@ import { PaymentModule } from './modules/payment/payment.module';
     PaymentModule,
   ],
   providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
     {
       provide: APP_GUARD,
       useClass: AccessTokenGuard,
